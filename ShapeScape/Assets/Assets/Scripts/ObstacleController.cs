8ce87cfc8816c4fe721f475f4536caf454db71f3
Ren-David Dimen <rendaviddimen@gmail.com>
2018-06-30 22:16:17 -0400

    create obstacle controller class to adjust obstacle attributes

---
+++ ShapeScape/Assets/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+    // public variables
+    public LayerMask obstaclesLayer;
+    public Transform parentObject;
+
+    // private variables;
+    private Rigidbody2D[] childRigidbody;
+    private float startSpeed = 2.0f;
+
+	void Start () {
+        childRigidbody = GetComponentsInChildren<Rigidbody2D>();
+
+        print(parentObject.position);
+    }
+	
+	void Update () {
+
+        foreach (Rigidbody2D blockade in childRigidbody) {
+            blockade.velocity = new Vector2(0.0f, -startSpeed);
+        }
+	}
+}
