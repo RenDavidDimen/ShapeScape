33b25334aa2009eec26824067d354a8a0f2a8254
Ren-David Dimen <rendaviddimen@gmail.com>
2018-07-03 01:27:34 -0400

    create new script to destroy platforms after reaching a certain y position

---
+++ ShapeScape/Assets/Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour {
+
+    public float destructionPoint = -10.0f;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.y <= destructionPoint)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
