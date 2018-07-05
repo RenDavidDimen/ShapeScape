c895e9dcf96c31fedf10be893cb7b4ae709dcf5c
Ren-David Dimen <rendaviddimen@gmail.com>
2018-07-05 17:10:00 -0400

    update destruction point in DestroySelf script

--- ShapeScape/Assets/Assets/Scripts/DestroySelf.cs
+++ ShapeScape/Assets/Assets/Scripts/DestroySelf.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class DestroySelf : MonoBehaviour {
 
-    public float destructionPoint = -10.0f;
+    public float deactivationPoint = -10.0f;
 
 	// Update is called once per frame
 	void Update () {
 
-        if (transform.position.y <= destructionPoint)
+        if (transform.position.y <= deactivationPoint)
         {
             Destroy(gameObject);
         }
