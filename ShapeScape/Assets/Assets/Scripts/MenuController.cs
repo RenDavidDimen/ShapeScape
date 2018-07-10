a1eb517bea66b5cdeca8abcb1c3642206ff8019a
Ren-David Dimen <rendaviddimen@gmail.com>
2018-07-09 22:08:07 -0400

    create controller for main menu interface

---
+++ ShapeScape/Assets/Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    private string gameScene = "GameScene";
+
+    public void Play() {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
