f13fdaea9f6cf595f9f185c4065fac30e5ed363c
Ren-David Dimen <rendaviddimen@gmail.com>
2018-07-09 22:10:42 -0400

    add DeathMenuController to implement pause menu after player death

---
+++ ShapeScape/Assets/Assets/Scripts/DeathMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenuController : MonoBehaviour {
+
+    public string mainMenu = "MainMenu";
+    public GameManager gameManager;
+
+    public void RestartGame() {
+        gameManager.Reset();
+    }
+
+    public void QuitToMain() {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
