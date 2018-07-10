51499241120d05831729b2b584a4eda9288b74b1
Ren-David Dimen <rendaviddimen@gmail.com>
2018-07-09 22:08:49 -0400

    add score and high score capabilities

---
+++ ShapeScape/Assets/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+    
+    public Text scoreText;
+    public Text highScoreText;
+    private int score;
+    private int highScore;
+    private string hsKey = "HighScore";
+
+	public void Start () {
+        score = 0;
+        if (PlayerPrefs.HasKey(hsKey)) {
+            highScore = PlayerPrefs.GetInt(hsKey);
+        }
+        PrintScore();
+	}
+	
+    public void UpdateScore() {
+        score++;
+
+        if (score > highScore) {
+            PlayerPrefs.SetInt(hsKey, score);
+            highScore = score;
+        }
+
+        PrintScore();
+    }
+
+    void PrintScore() {
+        scoreText.text = score.ToString();
+        highScoreText.text = "High Score: " + highScore.ToString();
+    }
+}
